ecdc2292ebe49c39871635e75c44701644537b00
ketap19 <ketanp170@gmail.com>
2023-04-18 19:45:54 +0530

    initial

---
+++ src/main/java/com/kdp/KdpApplication.java
@@ -0,0 +1,13 @@
+package com.kdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdpApplication.class, args);
+	}
+
+}
